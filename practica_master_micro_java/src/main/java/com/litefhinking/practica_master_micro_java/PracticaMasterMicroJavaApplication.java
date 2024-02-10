a29e56daa9af6df9f71f73890c6b8b0f14b5d272
redinx18 <redinx18@gmail.com>
2024-02-10 15:40:54 -0400

    Inicializar codigo fuente al repo
    Inicializar codigo fuente al repo

---
+++ practica_master_micro_java/src/main/java/com/litefhinking/practica_master_micro_java/PracticaMasterMicroJavaApplication.java
@@ -0,0 +1,13 @@
+package com.litefhinking.practica_master_micro_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaMasterMicroJavaApplication {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola, mi nobre es: Rehadynd Hilario");
+		SpringApplication.run(PracticaMasterMicroJavaApplication.class, args);
+	}
+}
---
+++ practica_master_micro_java/src/test/java/com/litefhinking/practica_master_micro_java/PracticaMasterMicroJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litefhinking.practica_master_micro_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaMasterMicroJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
